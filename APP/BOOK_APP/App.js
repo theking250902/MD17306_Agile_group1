4873be69c348c31c1f47fed1ea148677af2cc90e
theking250902 <buivanvietx5hongloc@gmail.com>
2023-05-21 10:27:28 +0700

    main

---
+++ APP/BOOK_APP/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView, View
+} from 'react-native';
+
+
+
+
+
+const App = () => {
+ return(
+  <View>
+    
+  </View>
+ )
+};
+
+
+
+export default App;
